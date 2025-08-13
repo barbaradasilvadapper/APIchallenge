5f7cbb79c33dd2589471be24d09ae6952b50a01a
Barbara Dapper <barbaradasilvadapper@gmail.com>
2025-08-13 16:10:45 -0300

    pastas

---
+++ APIchallenge/APIchallenge/Components/component.swift
@@ -0,0 +1,18 @@
+//
+//  component.swift
+//  APIchallenge
+//
+//  Created by Bárbara Dapper on 13/08/25.
+//
+
+import SwiftUI
+
+struct component: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    component()
+}
